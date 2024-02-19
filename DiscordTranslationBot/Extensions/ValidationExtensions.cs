ca5158157889f55f5957797f56b28b50205b6c0a
austins <austins@users.noreply.github.com>
2024-02-18 23:51:44 -0800

    Add comment

--- DiscordTranslationBot/Extensions/ValidationExtensions.cs
+++ DiscordTranslationBot/Extensions/ValidationExtensions.cs
@@ -4,6 +4,16 @@ namespace DiscordTranslationBot.Extensions;
 
 public static class ValidationExtensions
 {
+    /// <summary>
+    /// Validates an object instance with any Data Annotations validation attributes, and the
+    /// <see cref="IValidatableObject.Validate" /> method if it implements this.
+    /// </summary>
+    /// <param name="instance">The instance to validate.</param>
+    /// <param name="validationResults">The resulting validation results; empty if instance is valid.</param>
+    /// <returns>
+    /// true if valid or if the object has no validation attributes and isn't an <see cref="IValidatableObject" />;
+    /// false if invalid.
+    /// </returns>
     public static bool TryValidateObject(this object instance, out IReadOnlyList<ValidationResult> validationResults)
     {
         var results = new List<ValidationResult>();
