281c9beb9ccb06f945f95a2301615e1df2a07b06
austins <austins@users.noreply.github.com>
2024-02-23 14:51:42 -0800

    Add health checks

---
+++ DiscordTranslationBot.Tests/HealthChecks/DiscordClientHealthCheckTests.cs
@@ -0,0 +1,85 @@
+﻿using Discord;
+using DiscordTranslationBot.HealthChecks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace DiscordTranslationBot.Tests.HealthChecks;
+
+public sealed class DiscordClientHealthCheckTests
+{
+#pragma warning disable NUnit1032
+    private readonly IDiscordClient _client;
+#pragma warning restore NUnit1032
+    private readonly DiscordClientHealthCheck _sut;
+
+    public DiscordClientHealthCheckTests()
+    {
+        _client = Substitute.For<IDiscordClient>();
+        _sut = new DiscordClientHealthCheck(_client);
+    }
+
+    [Test]
+    public async Task CheckHealthAsync_Returns_Healthy()
+    {
+        // Arrange
+        _client.ConnectionState.Returns(ConnectionState.Connected);
+
+        var guilds = new List<IGuild> { Substitute.For<IGuild>() };
+        _client.GetGuildsAsync().ReturnsForAnyArgs(guilds);
+
+        var expectedData = new Dictionary<string, object>
+        {
+            { nameof(IDiscordClient.ConnectionState), _client.ConnectionState },
+            { "GuildCount", guilds.Count }
+        };
+
+        // Act
+        var result = await _sut.CheckHealthAsync(new HealthCheckContext(), CancellationToken.None);
+
+        // Assert
+        result.Status.Should().Be(HealthStatus.Healthy);
+        result.Data.Should().BeEquivalentTo(expectedData);
+    }
+
+    [TestCase(ConnectionState.Connecting)]
+    [TestCase(ConnectionState.Disconnecting)]
+    public async Task CheckHealthAsync_Returns_Degraded(ConnectionState state)
+    {
+        // Arrange
+        _client.ConnectionState.Returns(state);
+
+        var expectedData = new Dictionary<string, object>
+        {
+            { nameof(IDiscordClient.ConnectionState), _client.ConnectionState }
+        };
+
+        // Act
+        var result = await _sut.CheckHealthAsync(new HealthCheckContext(), CancellationToken.None);
+
+        // Assert
+        result.Status.Should().Be(HealthStatus.Degraded);
+        result.Data.Should().BeEquivalentTo(expectedData);
+
+        await _client.DidNotReceiveWithAnyArgs().GetGuildsAsync();
+    }
+
+    [Test]
+    public async Task CheckHealthAsync_Returns_Unhealthy()
+    {
+        // Arrange
+        _client.ConnectionState.Returns(ConnectionState.Disconnected);
+
+        var expectedData = new Dictionary<string, object>
+        {
+            { nameof(IDiscordClient.ConnectionState), _client.ConnectionState }
+        };
+
+        // Act
+        var result = await _sut.CheckHealthAsync(new HealthCheckContext(), CancellationToken.None);
+
+        // Assert
+        result.Status.Should().Be(HealthStatus.Unhealthy);
+        result.Data.Should().BeEquivalentTo(expectedData);
+
+        await _client.DidNotReceiveWithAnyArgs().GetGuildsAsync();
+    }
+}
---
+++ DiscordTranslationBot/HealthChecks/DiscordClientHealthCheck.cs
@@ -0,0 +1,39 @@
+﻿using Discord;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace DiscordTranslationBot.HealthChecks;
+
+public sealed class DiscordClientHealthCheck : IHealthCheck
+{
+    public const string HealthCheckName = "DiscordClient";
+
+    private readonly IDiscordClient _client;
+
+    public DiscordClientHealthCheck(IDiscordClient client)
+    {
+        _client = client;
+    }
+
+    public async Task<HealthCheckResult> CheckHealthAsync(
+        HealthCheckContext context,
+        CancellationToken cancellationToken = default)
+    {
+        var data = new Dictionary<string, object>
+        {
+            { nameof(IDiscordClient.ConnectionState), _client.ConnectionState }
+        };
+
+        if (_client.ConnectionState is ConnectionState.Connected)
+        {
+            data.Add(
+                "GuildCount",
+                (await _client.GetGuildsAsync(options: new RequestOptions { CancelToken = cancellationToken })).Count);
+
+            return HealthCheckResult.Healthy(data: data);
+        }
+
+        return _client.ConnectionState is ConnectionState.Connecting or ConnectionState.Disconnecting
+            ? HealthCheckResult.Degraded(data: data)
+            : HealthCheckResult.Unhealthy(data: data);
+    }
+}
