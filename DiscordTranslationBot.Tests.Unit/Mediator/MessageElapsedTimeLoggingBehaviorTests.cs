a0698b061c349d8aee20d023bda3e6d6f4d8b3e6
austins <austins@users.noreply.github.com>
2024-04-10 22:53:42 -0700

    Update test

--- DiscordTranslationBot.Tests.Unit/Mediator/MessageElapsedTimeLoggingBehaviorTests.cs
+++ DiscordTranslationBot.Tests.Unit/Mediator/MessageElapsedTimeLoggingBehaviorTests.cs
@@ -18,22 +18,22 @@ public sealed class MessageElapsedTimeLoggingBehaviorTests
     public async Task Handle_Success_Logs()
     {
         // Arrange
-        var request = Substitute.For<IMessage>();
+        var message = Substitute.For<IMessage>();
 
         // Act
-        await _sut.Handle(request, (_, _) => ValueTask.FromResult(true), CancellationToken.None);
+        await _sut.Handle(message, (_, _) => ValueTask.FromResult(true), CancellationToken.None);
 
         // Assert
         _logger.Entries.Count.Should().Be(2);
 
-        var requestName = request.GetType().Name;
+        var messageName = message.GetType().Name;
 
         var executingLog = _logger.Entries[0];
         executingLog.LogLevel.Should().Be(LogLevel.Information);
-        executingLog.Message.Should().Be($"Executing request '{requestName}'...");
+        executingLog.Message.Should().Be($"Executing message '{messageName}'...");
 
         var executedLog = _logger.Entries[1];
         executedLog.LogLevel.Should().Be(LogLevel.Information);
-        executedLog.Message.Should().StartWith($"Executed request '{requestName}'. Elapsed time:");
+        executedLog.Message.Should().StartWith($"Executed message '{messageName}'. Elapsed time:");
     }
 }
